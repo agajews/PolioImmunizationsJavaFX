ad195595e98c55d9c51ba7318ec2fd7a5f4ee09e
agajews <agajews@gmail.com>
2016-04-25 15:28:31 -0500

    Added filter, file menu

--- src/polioimmunizations/FXMLDocumentController.java
+++ src/polioimmunizations/FXMLDocumentController.java
@@ -9,10 +9,12 @@ import com.google.gson.*;
 import java.net.*;
 import java.util.*;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.chart.*;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -23,8 +25,40 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private BarChart<String, Number> chart;
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @FXML
+    private Slider min;
+    
+    @FXML
+    private Slider max;
+    
+    @FXML
+    private Label minLabel;
+    
+    @FXML
+    private Label maxLabel;
+    
+    private Dataset data;
+    
+    @FXML
+    private void handleChangeFilter(MouseEvent event) {
+        System.out.println("Clicked slider");
+        graphData(min.getValue(), max.getValue());
+        minLabel.setText("Minimum " + Math.round(min.getValue()) + "%");
+        maxLabel.setText("Maximum " + Math.round(max.getValue()) + "%");
+    }
+    
+    @FXML
+    private void handleClose(Event event) {
+        System.out.println("Clicked close");
+        System.exit(0);
+    }
+    
+    @FXML
+    private void handleHelp(Event event) {
+        System.out.println("Clicked help");
+    }
+    
+    private Dataset getData(){
         String s = "http://apps.who.int/gho/athena/data/GHO/WHS4_544.json?profile=simple&filter=YEAR:1980";
         URL address = null;
         try {
@@ -51,7 +85,10 @@ public class FXMLDocumentController implements Initializable {
 
         Gson gson = new Gson();
         Dataset dataset = gson.fromJson(str, Dataset.class);
-        
+        return dataset;
+    }
+    
+    private void graphData(double minVal, double maxVal){
         XYChart.Series<String, Number> immunizations = new XYChart.Series();
         immunizations.setName("immunizations");
         chart.setTitle("Polio Immunizations");
@@ -61,20 +98,29 @@ public class FXMLDocumentController implements Initializable {
         TreeSet<String> countries = new TreeSet();
         
         System.out.println("Polio Immunizations");
-        Location[] locations = dataset.getLocations();
+        Location[] locations = data.getLocations();
         for(Location location : locations){
             String country = location.getDim().getCountry();
             int value = location.getValue();
-            System.out.println(country + ": " + value + "%");
             if(country != null && !countries.contains(country)){
-                System.out.println(value);
-                immunizations.getData().add(new XYChart.Data(country, value));
-                countries.add(country);
+                if (value >= minVal && value <= maxVal){
+                    System.out.println(country + ": " + value + "%");
+                    immunizations.getData().add(new XYChart.Data(country, value));
+                    countries.add(country);
+                }
             }
         }
         
-        chart.getData().add(immunizations);
+        chart.getData().setAll(immunizations);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        data = getData();
         
+        graphData(0, 100);
+        
+        System.out.println(min.getValue());
         
     }    
     
